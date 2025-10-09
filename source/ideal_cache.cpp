9239e8e1e81f9d6e7360ee4e628d7161b4fffc86
ZEVS1206 <egor_bud5@inbox.ru>
2025-10-09 15:15:31 +0300

    Fixed some bugs in ideal cache. Added some tests

--- source/ideal_cache.cpp
+++ source/ideal_cache.cpp
@@ -7,50 +7,77 @@
 
 #include "cache_2q.h"
 
-int simulate_optimal_cache(const std::vector <Page_t> data, size_t data_size, size_t cache_size) 
+int simulate_optimal_cache(const std::vector<Page_t> data, size_t data_size, size_t cache_size)
 {
     std::unordered_set<Page_t> cache;
     int hits = 0;
 
-    for (size_t index = 0; index < data_size; index++) 
+    for (size_t index = 0; index < data_size; index++)
     {
-        int page = data[index];
-        if (cache.find(page) != cache.end()) {
+        Page_t page = data[index];
+
+        // hit
+        if (cache.find(page) != cache.end())
+        {
             hits++;
             continue;
         }
-        // miss
 
-        if (cache.size() < cache_size) 
+        // miss
+        bool will_be_used_later = false;
+        for (size_t j = index + 1; j < data_size; j++)
+        {
+            if (data[j] == page)
+            {
+                will_be_used_later = true;
+                break;
+            }
+        }
+
+        if (!will_be_used_later)
+        {
+            continue;
+        }
+
+        if (cache.size() < cache_size)
         {
             cache.insert(page);
-        } 
-        else 
+            continue;
+        }
+
+        Page_t victim = -1;
+        size_t farthest = 0;
+
+        for (Page_t c : cache)
         {
-            Page_t victim = -1;
-            size_t farthest = 0;
-            for (Page_t c : cache) 
+            size_t j = index + 1;
+            for (; j < data_size; j++)
             {
-                size_t j = index + 1;
-                for (; j < data_size; j++) 
+                if (data[j] == c)
                 {
-                    if (data[j] == c) 
-                    {
-                        break;
-                    }
-                }
-                if (j > farthest) 
-                {
-                    farthest = j;
-                    victim = c;
+                    break;
                 }
             }
-            cache.erase(victim);
-            cache.insert(page);
+
+            if (j == data_size)
+            {
+                victim = c;
+                break;
+            }
+
+            if (j > farthest)
+            {
+                farthest = j;
+                victim = c;
+            }
         }
+
+        cache.erase(victim);
+        cache.insert(page);
     }
     return hits;
 }
+
 
 
 int main(int argc, char *argv[])
@@ -87,6 +114,7 @@ int main(int argc, char *argv[])
                 return 1;
             }
         }
+
 
         size_t hits = simulate_optimal_cache(data_array, n, cache_size);
 
