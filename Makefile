6e2175e9cf68a3fd81e61e1233fbebea3ecc707a
ZEVS1206 <egor_bud5@inbox.ru>
2025-10-22 00:48:22 +0300

    Optimised cache_2q, added opportunity for testing with valgrind

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 CMAKE = cmake
 BUILD_DIR = build
+INCLUDE = -I include
+SOURCES = source/2q_main.cpp
 EXEC_DIR_2Q = build/cache_2q
 EXEC_DIR_IDEAL = build/ideal_cache
+
+PROFILE_CFLAGS = -ggdb3 -std=c++17 -O0 -Wall -Wextra -fno-omit-frame-pointer
+PROFILE_CFLAGS += -march=native -fno-pie
+PROFILER_OUT_NAME = callgrind.out
 
 file ?=
 
@@ -30,3 +36,15 @@ run_ideal: build
 	else \
 		./$(EXEC_DIR_IDEAL); \
 	fi
+
+
+profile_build:
+	g++ $(PROFILE_CFLAGS) $(INCLUDE) $(SOURCES) -o $(EXEC_DIR_2Q)_profile -no-pie
+
+callgrind: profile_build
+	@if [ -n "$(file)" ]; then \
+		/usr/bin/time -v valgrind --tool=callgrind --callgrind-out-file=$(PROFILER_OUT_NAME) ./$(EXEC_DIR_2Q)_profile $(file); \
+	else \
+		 /usr/bin/time -v valgrind --tool=callgrind --callgrind-out-file=$(PROFILER_OUT_NAME) ./$(EXEC_DIR_2Q)_profile; \
+	fi
+	@echo "Callgrind data saved to $(PROFILER_OUT_NAME). Use: kcachegrind $(PROFILER_OUT_NAME)"
--- include/cache_2q.h
+++ include/cache_2q.h
@@ -15,16 +15,29 @@
 #define ON_DEBUG(...)
 #endif
 
+enum Zone
+{
+    UNKNOWN = 0,
+    IN      = 1,
+    LRU     = 2,
+    OUT     = 3
+};
+
 template <typename Elem_t, typename Key_t, typename Function_get_elem_t>
 struct Cache_2q
 {
-    std::list<std::pair<Key_t, Elem_t>> buffer_in_;
-    std::list<std::pair<Key_t, Elem_t>> buffer_lru_;
-    std::list<Key_t> buffer_out_;
+    struct Cache_Entry
+    {
+        typename std::list<Key_t>::iterator it_key_list;
+        Elem_t value;
+        Zone zone = UNKNOWN;
+    };
 
-    std::unordered_map<Key_t, typename std::list<std::pair<Key_t, Elem_t>>::iterator> hash_table_in_;
-    std::unordered_map<Key_t, typename std::list<std::pair<Key_t, Elem_t>>::iterator> hash_table_lru_;
-    std::unordered_map<Key_t, typename std::list<Key_t>::iterator> hash_table_out_;
+    std::list <Key_t> buffer_in_;
+    std::list <Key_t> buffer_lru_;
+    std::list <Key_t> buffer_out_;
+
+    std::unordered_map<Key_t, Cache_Entry> common_hash_table_;
 
     size_t size_of_buffer_in_  = 0;
     size_t size_of_buffer_lru_ = 0;
@@ -35,27 +48,20 @@ struct Cache_2q
         ON_DEBUG(std::cout << "Common size of buffer: " << size_of_cache << "\n";)
 
         if (size_of_cache == 1)
-        {
             size_of_buffer_in_ = size_of_cache;
-        }
         else
-        {
             size_of_buffer_in_ = size_of_cache / 2;
-        }
+
         ON_DEBUG(std::cout << "Buffer in size: " << size_of_buffer_in_ <<  "\n";)
 
         size_of_buffer_lru_ = size_of_cache;
         ON_DEBUG(std::cout << "Buffer lru size: " << size_of_buffer_lru_ << "\n";)
 
-
         if (size_of_cache == 1)
-        {
             size_of_buffer_out_ = size_of_cache;
-        }
         else
-        {
             size_of_buffer_out_ = size_of_cache / 2;
-        }
+
         ON_DEBUG(std::cout << "Buffer out size: " << size_of_buffer_out_ << "\n";)
     }
 
@@ -63,100 +69,138 @@ struct Cache_2q
     {
         ON_DEBUG(std::cout << "Analyzing started!\n";)
 
-        auto hit_in_buffer_in = hash_table_in_.find(key);
-        if (hit_in_buffer_in != hash_table_in_.end())
+        auto it_in_table = common_hash_table_.find(key);
+        if (it_in_table != common_hash_table_.end())
         {
-            ON_DEBUG(std::cout << "Found in buffer_in\n";)
-            return true;
+            Cache_Entry &element = it_in_table->second;
+
+            if (element.zone == IN)
+            {
+                ON_DEBUG(std::cout << "Found in buffer_in\n";)
+                return true;
+            }
+            else if (element.zone == LRU)
+            {
+                ON_DEBUG(std::cout << "Found in buffer_lru\n";)
+                buffer_lru_.splice(buffer_lru_.begin(), buffer_lru_, element.it_key_list);
+                element.it_key_list = buffer_lru_.begin();
+                return true;
+            }
+            else if (element.zone == OUT)
+            {
+                ON_DEBUG(std::cout << "Found in buffer_out\n";)
+                delete_from_out(&element);
+                add_to_lru(key, &element);
+                return true;
+            }
         }
 
-        auto hit_in_buffer_lru = hash_table_lru_.find(key);
-        if (hit_in_buffer_lru != hash_table_lru_.end())
-        {
-            ON_DEBUG(std::cout << "Found in buffer_lru\n";)
-            buffer_lru_.splice(buffer_lru_.begin(), buffer_lru_, hit_in_buffer_lru->second);
-            hash_table_lru_[key] = buffer_lru_.begin();
-            return true;
-        }
-
-        auto hit_in_buffer_out = hash_table_out_.find(key);
-        if (hit_in_buffer_out != hash_table_out_.end())
-        {
-            ON_DEBUG(std::cout << "Found in buffer_out\n";)
-            add_to_lru(key, slow_get_page(key));
-            delete_from_out(hit_in_buffer_out->second);
-            return true;
-        }
-
-        add_to_in(key, slow_get_page(key));
+        add_to_in(key, slow_get_page(key), nullptr);
         ON_DEBUG(std::cout << "Not found, added to buffer_in\n";)
         return false;
     }
 
-    void add_to_lru(Key_t key, Elem_t element)
+    void add_to_lru(Key_t key, Cache_Entry *element)
     {
         if (buffer_lru_.size() >= size_of_buffer_lru_)
         {
             delete_from_lru();
         }
 
-        buffer_lru_.push_front(std::make_pair(key, element));
-        hash_table_lru_[key] = buffer_lru_.begin();
+        buffer_lru_.push_front(key);
+
+        if (element != nullptr)
+        {
+            element->it_key_list = buffer_lru_.begin();
+            element->zone = LRU;
+        }
+        else
+        {
+            Cache_Entry new_entry;
+            new_entry.value = Elem_t();
+            new_entry.zone = LRU;
+            new_entry.it_key_list = buffer_lru_.begin();
+            common_hash_table_[key] = std::move(new_entry);
+        }
     }
 
-    void add_to_in(Key_t key, Elem_t element)
+    void add_to_in(Key_t key, Elem_t elem, Cache_Entry * /*element*/)
     {
         if (buffer_in_.size() >= size_of_buffer_in_)
         {
-            auto old = buffer_in_.back();
+            Key_t old = buffer_in_.back();
             delete_from_in();
-            add_to_out(old.first);
+            add_to_out(old);
         }
 
-        buffer_in_.push_front(std::make_pair(key, element));
-        hash_table_in_[key] = buffer_in_.begin();
+        buffer_in_.push_front(key);
+
+        Cache_Entry new_element;
+        new_element.value = std::move(elem);
+        new_element.zone = IN;
+        new_element.it_key_list = buffer_in_.begin();
+
+        common_hash_table_[key] = std::move(new_element);
     }
 
     void add_to_out(Key_t key)
     {
-        if (buffer_out_.size() >= size_of_buffer_out_)
+        auto it_old = common_hash_table_.find(key);
+        if (it_old != common_hash_table_.end())
         {
-            
-            Key_t last = buffer_out_.back();
-            hash_table_out_.erase(last);
-            buffer_out_.pop_back();
+            if (buffer_out_.size() >= size_of_buffer_out_)
+            {
+                Key_t last = buffer_out_.back();
+                common_hash_table_.erase(last);
+                buffer_out_.pop_back();
+            }
+            it_old->second.zone = OUT;
+            buffer_out_.push_front(key);
+            it_old->second.it_key_list = buffer_out_.begin();
         }
-        buffer_out_.push_front(key);
-        hash_table_out_[key] = buffer_out_.begin();
     }
 
-    
-    void delete_from_out(typename std::list<Key_t>::iterator iterator_on_buffer_out)
+    void delete_from_out(Cache_Entry *element)
     {
-        hash_table_out_.erase(*iterator_on_buffer_out);
-        buffer_out_.erase(iterator_on_buffer_out);
+        if (element == nullptr) 
+        {
+            return;
+        }
+        buffer_out_.erase(element->it_key_list);
+        element->zone = UNKNOWN;
     }
 
     void delete_from_in()
     {
-        Key_t k = buffer_in_.back().first;
-        hash_table_in_.erase(k);
+        if (buffer_in_.empty()) 
+        {
+            return;
+        }
         buffer_in_.pop_back();
     }
 
     void delete_from_lru()
     {
-        Key_t k = buffer_lru_.back().first;
-        hash_table_lru_.erase(k);
+        if (buffer_lru_.empty())
+        {
+            return;
+        }
+
+        Key_t k = buffer_lru_.back();
+        common_hash_table_.erase(k);
         buffer_lru_.pop_back();
     }
 
     void print_buffer_in()
     {
         std::cout << "Buffer in: ";
-        for (auto &element: buffer_in_)
+        for (auto &k: buffer_in_)
         {
-            std::cout << "(" << element.first << ":" << element.second << ") ";
+            auto it = common_hash_table_.find(k);
+            if (it != common_hash_table_.end())
+                std::cout << "(" << k << ":" << it->second.value << ") ";
+            else
+                std::cout << "(" << k << ":?) ";
         }
         std::cout << "\n";
     }
@@ -164,9 +208,9 @@ struct Cache_2q
     void print_buffer_out()
     {
         std::cout << "Buffer out: ";
-        for (auto &element: buffer_out_)
+        for (auto &k: buffer_out_)
         {
-            std::cout << element << " ";
+            std::cout << k << " ";
         }
         std::cout << "\n";
     }
@@ -174,9 +218,13 @@ struct Cache_2q
     void print_buffer_lru()
     {
         std::cout << "Buffer lru: ";
-        for (auto &element: buffer_lru_)
+        for (auto &k: buffer_lru_)
         {
-            std::cout << "(" << element.first << ":" << element.second << ") ";
+            auto it = common_hash_table_.find(k);
+            if (it != common_hash_table_.end())
+                std::cout << "(" << k << ":" << it->second.value << ") ";
+            else
+                std::cout << "(" << k << ":?) ";
         }
         std::cout << "\n";
     }
@@ -185,6 +233,6 @@ struct Cache_2q
 
 typedef int Page_t;
 
-int simulate_optimal_cache(const std::vector <Page_t> data, size_t data_size, size_t cache_size);
+int simulate_optimal_cache(const std::vector<Page_t> data, size_t data_size, size_t cache_size);
 
 #endif
